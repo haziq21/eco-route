ab5507bbdf3af58b895e65abb6a18d223c71d600
Haziq Hairil <haziqhairil21@gmail.com>
2021-08-22 15:44:25 +0800

    Add box components

---
+++ src/lib/Box.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let title = undefined;
+</script>
+
+<div class="box">
+	{#if title}
+		<h1>{title}</h1>
+	{/if}
+	<slot />
+</div>
+
+<style>
+	.box {
+		background-color: var(--overlay);
+		color: var(--text);
+		box-shadow: var(--shadow);
+		border-radius: var(--border-radius);
+		padding: var(--space);
+		margin-bottom: var(--space);
+	}
+</style>
---
+++ src/lib/EmphasisedBox.svelte
@@ -0,0 +1,14 @@
+<div class="box">
+	<slot />
+</div>
+
+<style>
+	.box {
+		background-color: var(--header);
+		color: var(--overlay);
+		box-shadow: var(--shadow);
+		border-radius: var(--border-radius);
+		padding: var(--space);
+		margin-bottom: var(--space);
+	}
+</style>
