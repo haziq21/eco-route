2e6cbe1e8db56e5db77314d60004face23061f1a
Haziq Hairil <haziqhairil21@gmail.com>
2021-08-22 15:39:01 +0800

    Configure svelte static adapter and firebase

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,17 @@
+import adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		adapter: adapter({
+			pages: 'public',
+			assets: 'public',
+			fallback: 'index.html'
+		}),
+		ssr: false,
+		files: {
+			template: 'src/index.html'
+		},
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
