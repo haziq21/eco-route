2fcf895b64c7da878711e0c4296285a3e2968e9b
Haziq Hairil <haziqhairil21@gmail.com>
2021-08-22 15:57:21 +0800

    Add searchbar component

---
+++ src/lib/Searchbar.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let placeholder;
+	export let defaultText = '';
+	export let ref = undefined;
+</script>
+
+<input type="text" {placeholder} value={defaultText} bind:this={ref} on:click />
+
+<style>
+	input {
+		width: 100%;
+		padding: 9px;
+		/* margin: 0 auto; */
+		/* margin-bottom: 5px; */
+		background-color: var(--input-field);
+		color: var(--icon-text);
+		opacity: 1;
+		font-size: 1em;
+		border: none;
+		border-radius: var(--border-radius-sm);
+		box-shadow: var(--shadow);
+	}
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import EmphasisedBox from '$lib/EmphasisedBox.svelte';
+	import Searchbar from '$lib/Searchbar.svelte';
+	import { page } from '$app/stores';
+</script>
+
+<EmphasisedBox>
+	<!-- "Go somewhere" homepage header -->
+	{#if $page.path == '/'}
+		<h1>Go somewhere</h1>
+	{/if}
+
+	<!-- Destination searchbar -->
+	<Searchbar placeholder="Enter your destination" />
+</EmphasisedBox>
