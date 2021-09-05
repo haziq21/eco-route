a3c897d6cc845fb2f0742a61ab1c516aff364476
Haziq Hairil <haziqhairil21@gmail.com>
2021-09-05 10:33:27 +0800

    Include extra route information

--- src/routes/suggested-routes.svelte
+++ src/routes/suggested-routes.svelte
@@ -1,19 +1,9 @@
 <script lang="ts">
 	import Box from '$lib/Box.svelte';
-	import Route from '$lib/Route.svelte';
+	import RouteTimeline from '$lib/RouteTimeline.svelte';
+	import type { route } from 'src/types/directions.type.js';
 	import type { place } from 'src/types/places.type';
 	import { destinationQuery, originQuery, routes } from './stores.js';
-
-	function formatTime(seconds: number) {
-		const hours = Math.floor(seconds / 3600);
-		const minutes = Math.floor((seconds - hours * 3600) / 60);
-
-		if (hours) {
-			return `${hours} hr ${minutes} min`;
-		} else {
-			return `${minutes} min`;
-		}
-	}
 
 	function hours(seconds: number) {
 		return Math.floor(seconds / 3600);
@@ -35,47 +25,76 @@
 		return data;
 	}
 
-	// $routes = $routes.length ? $routes : getDirections($originQuery, $destinationQuery);
-	let directions = $routes.length
-		? $routes
-		: getDirections($originQuery, $destinationQuery).then(
-				(newDirections) => ($routes = newDirections)
-		  );
+	let directions: route[] | Promise<route[]> = $routes;
+	if (!directions.length && $originQuery.name && $destinationQuery.name) {
+		directions = getDirections($originQuery, $destinationQuery).then(
+			(newDirections) => ($routes = newDirections)
+		);
+	}
 </script>
 
 <Box>
-	{#await directions}
-		loading...
-	{:then response}
-		<div class="timelines">
-			{#each response as route}
-				<Route
-					{route}
-					longestRoute={response.reduce((a, b) => (a > (b = b.distance) ? a : b), 0)}
-				/>
-				<span class="time">
-					<!-- {formatTime(route.duration)} -->
-					{#if hours(route.duration)}
-						<span class="number">{hours(route.duration)}</span>
-						hr
-					{/if}
-					<span class="number">{minutes(route.duration)}</span>
-					min
-				</span>
-			{/each}
-		</div>
-	{:catch error}
-		Error: {error.message}
-	{/await}
+	{#if $originQuery.name && $destinationQuery.name}
+		{#await directions}
+			loading...
+		{:then response}
+			<div class="timelines">
+				{#each response as route}
+					<RouteTimeline
+						{route}
+						longestRoute={response.reduce((a, b) => (a > (b = b.distance) ? a : b), 0)}
+					/>
+
+					<!-- How long the route takes -->
+					<span class="time">
+						{#if hours(route.duration)}
+							<span class="number">{hours(route.duration)}</span>
+							hr
+						{/if}
+						<span class="number">{minutes(route.duration)}</span>
+						min
+					</span>
+
+					<!-- Extra information like walking and arrival time -->
+					<span class="extra">
+						Leave now, arrive at
+						<span class="number"
+							>{new Date(route.arriveTime).toLocaleString('en', {
+								hour: 'numeric',
+								minute: 'numeric'
+							})}</span
+						>.
+						<span class="number">{Math.round(route.walkTime / 60)}</span> min walking time.
+					</span>
+				{/each}
+			</div>
+		{:catch error}
+			Error: {error.message}
+		{/await}
+	{:else}
+		enter location to search
+	{/if}
 </Box>
 
 <style>
+	.extra {
+		/* font-weight: lighter; */
+		font-size: 0.8em;
+		grid-column: 1 / 3;
+		margin: 3px 0 var(--space) 0;
+	}
+
 	.timelines {
 		display: grid;
-		gap: var(--space);
+		column-gap: var(--space);
 		grid-template-columns: 1fr auto;
 		justify-content: space-between;
 		align-items: center;
+	}
+
+	.time {
+		font-size: 1.1em;
+		text-align: right;
 	}
 
 	.number {
