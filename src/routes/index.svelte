07f2527c356fd270a58e9868f6ae22c749d5e1ea
Haziq Hairil <haziqhairil21@gmail.com>
2021-09-07 12:26:54 +0800

    Disable SSR for index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+	export const ssr = false;
+</script>
+
 <script lang="ts">
 	import Box from '$lib/Box.svelte';
 	import BusArrivals from '$lib/BusArrivals.svelte';
@@ -34,17 +38,23 @@
 		latitude: null
 	};
 	$: $originQuery = $currentPlace;
+
+	let searchText: string;
 </script>
 
 <Box>
 	<h1>Bus arrivals</h1>
-	<Searchbar placeholder="Search for a bus number or stop" />
-	{#await getBusArrivals()}
-		<p>loading...</p>
-	{:then response}
-		<!-- <p>{JSON.stringify(response)}</p> -->
-		<BusArrivals arrivals={response.data} />
-	{:catch error}
-		<p>error: {error.message}</p>
-	{/await}
+	<Searchbar placeholder="Search for a bus number or stop" bind:text={searchText} />
+	{#if !searchText}
+		{#await getBusArrivals()}
+			<p>loading...</p>
+		{:then response}
+			<!-- <p>{JSON.stringify(response)}</p> -->
+			<BusArrivals arrivals={response.data} />
+		{:catch error}
+			<p>error: {error.message}</p>
+		{/await}
+	{:else}
+		text
+	{/if}
 </Box>
