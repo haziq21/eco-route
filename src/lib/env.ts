c4a30eabd4ce730101f8aad60a39362817551052
Haziq Hairil <haziqhairil21@gmail.com>
2021-09-08 18:39:52 +0800

    Add types for env variables

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,7 +1,7 @@
 const prefix = process.env.NODE_ENV === 'production' ? process.env : import.meta.env;
 
-export const DATAMALL_KEY = prefix.VITE_DATAMALL_KEY;
-export const ONEMAP_CREDENTIALS = JSON.stringify({
+export const DATAMALL_KEY: string = prefix.VITE_DATAMALL_KEY;
+export const ONEMAP_CREDENTIALS: string = JSON.stringify({
 	email: prefix.VITE_ONEMAP_EMAIL,
 	password: prefix.VITE_ONEMAP_PASSWORD
 });
