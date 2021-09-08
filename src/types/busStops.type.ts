968bbf800f804d87669c5b483d67df184252b505
Haziq Hairil <haziqhairil21@gmail.com>
2021-09-08 17:51:29 +0800

    Include types for location information in bus stops API

--- src/types/busStops.type.ts
+++ src/types/busStops.type.ts
@@ -1,9 +1,13 @@
 export interface busStop {
 	code: string;
 	name: string;
+	latitude: number;
+	longitude: number;
 }
 
 export interface rawBusStop {
 	BusStopCode: string;
 	Description: string;
+	Latitude: number;
+	Longitude: number;
 }
