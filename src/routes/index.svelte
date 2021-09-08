736527e3a059c3a1059ef3aaf1f0be07c9f625fa
Haziq Hairil <haziqhairil21@gmail.com>
2021-09-08 17:49:12 +0800

    Show arrival information for nearby bus stops

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,8 +7,13 @@
 	import Box from '$lib/Box.svelte';
 	import BusArrivals from '$lib/BusArrivals.svelte';
 	import Searchbar from '$lib/Searchbar.svelte';
+	import type { arrivals } from 'src/types/busArrivals.type';
+	import type { busStop } from 'src/types/busStops.type';
 	import { onMount } from 'svelte';
 	import { currentPlace, destinationQuery, originQuery } from './_stores';
+
+	// Content of bus number / bus stop searchbar
+	let searchText: string;
 
 	// Service worker registration
 	onMount(async () => {
@@ -20,18 +25,50 @@
 		}
 	});
 
-	// Fetch bus arrival timings
-	async function getBusArrivals(busStopCode: string) {
-		const res = await fetch(`/api/bus-arrivals?stop-code=${busStopCode}`);
-		const data = await res.json();
-		return data;
+	// Fetched bus arrivals at bus stops nearby
+	async function getNearbyArrivals() {
+		const nearbyBusStops = await getNearbyBusStops();
+		return Promise.all(nearbyBusStops.map(async (stop) => await getBusArrivals(stop.code)));
 	}
 
-	async function getBusStops() {
-		const res = await fetch('/api/bus-stops');
-		const data = await res.json();
+	// Fetches bus arrival timings at a bus stop
+	async function getBusArrivals(busStopCode: string): Promise<arrivals> {
+		const res = await fetch(`/api/bus-arrivals?stop-code=${busStopCode}`);
+		return await res.json();
+	}
 
-		return data;
+	// Fetches bus stops within 500 metres of user
+	async function getNearbyBusStops() {
+		const res = await fetch('/api/bus-stops');
+		const data: busStop[] = await res.json();
+
+		// Only return bus stops less than 0.5km away
+		return data.filter((stop) => distanceBetween($currentPlace, stop) <= 0.5);
+	}
+
+	// Gets distance between two latitude-longitude points in km (using the Haversine formula)
+	// From https://stackoverflow.com/a/27943
+	function distanceBetween(
+		{ latitude: lat1, longitude: lon1 },
+		{ latitude: lat2, longitude: lon2 }
+	) {
+		// Radius of the earth in km
+		const R = 6371;
+		const latDiff = toRad(lat2 - lat1);
+		const lonDiff = toRad(lon2 - lon1);
+
+		const a =
+			Math.sin(latDiff / 2) * Math.sin(latDiff / 2) +
+			Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) * Math.sin(lonDiff / 2) * Math.sin(lonDiff / 2);
+		const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+		// Distance in km
+		return R * c;
+	}
+
+	// Converts degrees to radians
+	function toRad(degrees: number) {
+		return degrees * (Math.PI / 180);
 	}
 
 	// Clear destination query when user goes back to home page
@@ -44,19 +81,18 @@
 
 	// Reset origin query to user's current location when they go back to home page
 	$: $originQuery = $currentPlace;
-
-	let searchText: string;
 </script>
 
 <Box>
 	<h1>Bus arrivals</h1>
 	<Searchbar placeholder="Search for a bus number or stop" bind:text={searchText} />
 	{#if !searchText}
-		{#await getBusArrivals('19049')}
+		{#await getNearbyArrivals()}
 			<p>loading...</p>
 		{:then response}
-			<!-- <p>{JSON.stringify(response)}</p> -->
-			<BusArrivals arrivals={response} />
+			{#each response as busStop}
+				<BusArrivals arrivals={busStop} />
+			{/each}
 		{:catch error}
 			<p>error: {error.message}</p>
 		{/await}
