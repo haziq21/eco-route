16cd6e1c4ce6f70846ef85f28299010d7ebdb729
Haziq Hairil <haziqhairil21@gmail.com>
2021-09-09 14:03:47 +0800

    Implement bus stop searching

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,12 +14,30 @@
 
 	// Content of bus number / bus stop searchbar
 	let searchText: string;
+	// Content of searchbar before searchText was updated
+	// (this is updated in searchBusStops())
+	let previousSearchText: string;
+
+	// Clear destination query when user goes back to home page
+	$destinationQuery = {
+		name: '',
+		address: '',
+		longitude: null,
+		latitude: null
+	};
+
+	// Reset origin query to user's current location when they go back to home page
+	$: $originQuery = $currentPlace;
+
+	// Search results
+	let searchResults: busStop[] = [];
+	$: if (searchText) searchBusStops(searchText).then((res) => (searchResults = res));
 
 	// Service worker registration
 	onMount(async () => {
 		if ('serviceWorker' in navigator) {
 			try {
-				const registration = await navigator.serviceWorker.register('/sw.js');
+				const registration = await navigator.serviceWorker.register('/service-worker.js');
 				console.log('ServiceWorker registration successful with scope: ', registration.scope);
 			} catch (e) {
 				console.log('ServiceWorker registration failed: ', e);
@@ -27,7 +45,7 @@
 		}
 	});
 
-	// Fetched bus arrivals at bus stops nearby
+	// Fetches bus arrivals at bus stops nearby
 	async function getNearbyArrivals() {
 		const nearbyBusStops = await getNearbyBusStops();
 		return Promise.all(nearbyBusStops.map(async (stop) => await getBusArrivals(stop.code)));
@@ -41,11 +59,49 @@
 
 	// Fetches bus stops within 500 metres of user
 	async function getNearbyBusStops() {
-		const res = await fetch('/api/bus-stops');
-		const data: busStop[] = await res.json();
+		const data = await getAllBusStops();
 
 		// Only return bus stops less than 0.5km away
 		return data.filter((stop) => distanceBetween($currentPlace, stop) <= 0.5);
+	}
+
+	// Fetches all bus stops
+	async function getAllBusStops() {
+		const res = await fetch('/api/bus-stops');
+		const data: busStop[] = await res.json();
+
+		return data;
+	}
+
+	// Searches for bus stops by name or code
+	async function searchBusStops(query: string) {
+		// Case-insensitive keyword search
+		const queries = query
+			.toLowerCase()
+			.split(' ')
+			.filter((x) => x.length);
+		const busStops = await getAllBusStops();
+		let results: { matches: number; stop: busStop }[] = [];
+
+		for (let i = 0; i < busStops.length; i++) {
+			// Count the number of keyword matches
+			const matches = queries.reduce((total, current) => {
+				if (busStops[i].name.toLowerCase().includes(current) || busStops[i].code.includes(current))
+					return total + 1;
+				return total;
+			}, 0);
+
+			// If it matches, store the bus stop and its number of matches
+			if (matches) {
+				results.push({ matches, stop: busStops[i] });
+			}
+		}
+
+		// Return the first 10 bus stops with the most matches
+		return results
+			.sort((stop1, stop2) => stop2.matches - stop1.matches)
+			.slice(0, 11)
+			.map((stop) => stop.stop);
 	}
 
 	// Gets distance between two latitude-longitude points in km (using the Haversine formula)
@@ -72,17 +128,6 @@
 	function toRad(degrees: number) {
 		return degrees * (Math.PI / 180);
 	}
-
-	// Clear destination query when user goes back to home page
-	$destinationQuery = {
-		name: '',
-		address: '',
-		longitude: null,
-		latitude: null
-	};
-
-	// Reset origin query to user's current location when they go back to home page
-	$: $originQuery = $currentPlace;
 </script>
 
 <Box>
@@ -99,6 +144,10 @@
 			<p>error: {error.message}</p>
 		{/await}
 	{:else}
-		text
+		{#each searchResults as busStop}
+			{busStop.name} {busStop.code}<br />
+		{:else}
+			No results
+		{/each}
 	{/if}
 </Box>
