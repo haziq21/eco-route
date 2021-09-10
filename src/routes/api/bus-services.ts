5b4fe2a46a67848353e2712cf758fe97bf417dca
Haziq Hairil <haziqhairil21@gmail.com>
2021-09-10 22:47:43 +0800

    Implement bus services API

---
+++ src/routes/api/bus-services.ts
@@ -0,0 +1,40 @@
+import { DATAMALL_KEY } from '$lib/env';
+import type { rawService, service } from 'src/types/busServices.type';
+
+export async function get(): Promise<{ body: service[] }> {
+	const services = await fetchData();
+	const formattedServices = [...new Set(services.map(formatService))];
+
+	return {
+		body: formattedServices
+	};
+}
+
+async function fetchData(iteration = 0) {
+	// DataMall API URL
+	const url = 'http://datamall2.mytransport.sg/ltaodataservice/BusServices';
+
+	// HTTP request
+	const res = await fetch(`${url}?$skip=${iteration * 500}`, {
+		headers: {
+			AccountKey: DATAMALL_KEY
+		}
+	});
+	let data: rawService[] = (await res.json()).value;
+
+	// Recurse to retrieve more data
+	if (data.length >= 500) {
+		const newData = await fetchData((iteration = iteration + 1));
+		data = data.concat(newData);
+	}
+
+	return data;
+}
+
+function formatService(data: rawService): service {
+	return {
+		number: data.ServiceNo,
+		origin: data.OriginCode,
+		destination: data.DestinationCode
+	};
+}
---
+++ src/types/busServices.type.ts
@@ -0,0 +1,11 @@
+export type service = {
+	number: string;
+	origin: string;
+	destination: string;
+};
+
+export type rawService = {
+	ServiceNo: string;
+	OriginCode: string;
+	DestinationCode: string;
+};
