134c39d0d0b95909737a5866de33a1699877e740
Haziq Hairil <haziqhairil21@gmail.com>
2021-09-17 23:17:48 +0800

    Include bus route data in build

--- src/routes/api/bus-routes/[busNumber].ts
+++ src/routes/api/bus-routes/[busNumber].ts
@@ -1,72 +1,77 @@
-import type { rawBusRoutePoint, busRoutes } from '$lib/types';
-import { DATAMALL_KEY } from '$lib/env';
-import fs from 'fs';
+// import type { rawBusRoutePoint, busRoutes } from '$lib/types';
+// import { DATAMALL_KEY } from '$lib/env';
+// import fs from 'fs';
+import busRouteDB from '$lib/busRoutes.json';
 
 // This takes about 10 seconds (51 recursions)
-async function fetchRawData(iteration = 0) {
-	// DataMall API URL
-	const dataMallUrl = 'http://datamall2.mytransport.sg/ltaodataservice/BusRoutes';
+// async function fetchRawData(iteration = 0) {
+// 	// DataMall API URL
+// 	const dataMallUrl = 'http://datamall2.mytransport.sg/ltaodataservice/BusRoutes';
 
-	// Send HTTP request
-	const res = await fetch(`${dataMallUrl}?$skip=${iteration * 500}`, {
-		headers: {
-			AccountKey: DATAMALL_KEY
-		}
-	});
-	let data: rawBusRoutePoint[] = (await res.json()).value;
+// 	// Send HTTP request
+// 	const res = await fetch(`${dataMallUrl}?$skip=${iteration * 500}`, {
+// 		headers: {
+// 			AccountKey: DATAMALL_KEY
+// 		}
+// 	});
+// 	let data: rawBusRoutePoint[] = (await res.json()).value;
 
-	// Recurse to retrieve more data
-	if (data.length >= 500) {
-		const newData = await fetchRawData((iteration = iteration + 1));
-		data = data.concat(newData);
-	}
+// 	// Recurse to retrieve more data
+// 	if (data.length >= 500) {
+// 		const newData = await fetchRawData((iteration = iteration + 1));
+// 		data = data.concat(newData);
+// 	}
 
-	// Return full data
-	return data;
-}
+// 	// Return full data
+// 	return data;
+// }
 
 export async function get({ params, query }) {
-	function cont() {
-		// Retrieve data
-		try {
-			const dataString = fs.readFileSync('busRoutes.json', 'utf8');
-			const fullData: busRoutes = JSON.parse(dataString);
-			const data = fullData[params.busNumber][`direction${query.get('direction')}`];
+	// function cont() {
+	// 	// Retrieve data
+	// 	try {
+	// 		const dataString = fs.readFileSync('busRoutes.json', 'utf8');
+	// 		const fullData: busRoutes = JSON.parse(dataString);
+	// 		const data = fullData[params.busNumber][`direction${query.get('direction')}`];
 
-			// Filter bus routes by service number and route direction
-			return { body: data };
-		} catch (err) {
-			console.log(err);
-			return;
-		}
-	}
+	// 		// Filter bus routes by service number and route direction
+	// 		return { body: data };
+	// 	} catch (err) {
+	// 		console.log(err);
+	// 		return;
+	// 	}
+	// }
 
-	// Create busRoutes.json if it has not yet been created
-	if (!fs.existsSync('busRoutes.json')) {
-		const data = await fetchRawData();
-		const formattedData = formatBusRoutes(data);
-		fs.writeFile('busRoutes.json', JSON.stringify(formattedData), (err) => {
-			if (err) console.log(err);
-		});
-	} else return cont();
+	// // Create busRoutes.json if it has not yet been created
+	// if (!fs.existsSync('busRoutes.json')) {
+	// 	const data = await fetchRawData();
+	// 	const formattedData = formatBusRoutes(data);
+	// 	fs.writeFile('busRoutes.json', JSON.stringify(formattedData), (err) => {
+	// 		if (err) console.log(err);
+	// 	});
+	// } else return cont();
+
+	return {
+		body: busRouteDB[params.busNumber][`direction${query.get('direction')}`]
+	};
 }
 
-function formatBusRoutes(busRoutes: rawBusRoutePoint[]): busRoutes {
-	const formattedBusRoutes: busRoutes = {};
+// function formatBusRoutes(busRoutes: rawBusRoutePoint[]): busRoutes {
+// 	const formattedBusRoutes: busRoutes = {};
 
-	for (let i = 0; i < busRoutes.length; i++) {
-		const serviceNumber = busRoutes[i].ServiceNo;
-		const routeDirection = `direction${busRoutes[i].Direction}`;
-		const stopCode = busRoutes[i].BusStopCode;
+// 	for (let i = 0; i < busRoutes.length; i++) {
+// 		const serviceNumber = busRoutes[i].ServiceNo;
+// 		const routeDirection = `direction${busRoutes[i].Direction}`;
+// 		const stopCode = busRoutes[i].BusStopCode;
 
-		// Initialise empty bus route if needed
-		if (!(serviceNumber in formattedBusRoutes)) {
-			formattedBusRoutes[serviceNumber] = { direction1: [], direction2: [] };
-		}
+// 		// Initialise empty bus route if needed
+// 		if (!(serviceNumber in formattedBusRoutes)) {
+// 			formattedBusRoutes[serviceNumber] = { direction1: [], direction2: [] };
+// 		}
 
-		// Add this bus stop code to the list
-		formattedBusRoutes[serviceNumber][routeDirection].push(stopCode);
-	}
+// 		// Add this bus stop code to the list
+// 		formattedBusRoutes[serviceNumber][routeDirection].push(stopCode);
+// 	}
 
-	return formattedBusRoutes;
-}
+// 	return formattedBusRoutes;
+// }
